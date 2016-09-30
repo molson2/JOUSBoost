e35d794d44d58ac0f2a31ef3a1acb2a195d154db
molson2 <molson5574@gmail.com>
2016-09-30 14:27:23 -0400

    fixed circle example to sample from full square

--- R/simulation.R
+++ R/simulation.R
@@ -50,7 +50,7 @@ circle_data = function(n = 500, inner_r = 8, outer_r = 28){
   if(outer_r <= inner_r)
     stop('outer_r must be strictly larger than inner_r')
 
-  X = matrix(stats::runif(2*n, 0, outer_r), nrow=n, ncol=2)
+  X = matrix(stats::runif(2*n, -outer_r, outer_r), nrow=n, ncol=2)
   r = apply(X, 1, function(x) sqrt(sum(x^2)))
   p = 1*(r < inner_r) +
     (outer_r-r)/(outer_r-inner_r)*((inner_r < r) & (r < outer_r))
